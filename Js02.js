362fea0e83f944536201755673c53431262414af
gzv007 <rpg.668@gmail.com>
2022-07-04 23:35:44 +0700

    สลากกินแบ่ง 01

---
+++ Js02.js
@@ -0,0 +1,11 @@
+var name = "hello"
+console.log("Hello Oat");
+alert(name)
+alert(typeof(name))
+var x = 1
+{
+    var y = 2
+    let z = 3 
+    // let จะสามารถกำหนดขอบเขตได้
+}
+alert(z)
---
+++ lotto.js
@@ -0,0 +1,5 @@
+lot = Math.floor(Math.random()*1000)
+console.log(lot);
+x = prompt("กรอกเลขของคุณ")
+alert("หมายเลขของคุณคือ "+ x)
+document.getElementById("result").innerHTML = lot
